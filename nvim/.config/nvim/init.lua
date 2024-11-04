db4bb4476ec7bfeec452ba7dff28c95ada17d27a
Tiankai Xie <txie21@en4146049l.fulton.ad.asu.edu>
2024-11-04 14:23:04 -0700

    sync

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,19 +1,2 @@
-require('user.base')
-require('user.highlights')
-require('user.maps')
-require('user.plugins')
-
-local has = vim.fn.has
-local is_mac = has "macunix"
-local is_win = has "win32"
-local is_wsl = has "wsl"
-
-if is_mac then
-  require('user.macos')
-end
-if is_win then
-  require('user.windows')
-end
-if is_wsl then
-  require('user.wsl')
-end
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
---
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,14 @@
+return {
+  "craftsdog/solarized-osaka.nvim",
+  lazy = true,
+  priority = 1000,
+  opts = function()
+    return {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    }
+  end,
+}
